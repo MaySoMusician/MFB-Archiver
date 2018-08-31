0f239dd7fcb57e5fca2340709badeb5ad26fc4be
MaySoMusician <maysomusician@gmail.com>
2018-08-31 23:29:40 +0900

    Cooking module now outputs error log

--- modules/cooking.js
+++ modules/cooking.js
@@ -12,24 +12,31 @@ module.exports = (MFBAcvr) => {
   MFBAcvr.cooker.cmds = {};
   
   MFBAcvr.cooker.cmds.cook = (rID) => {
-    let output = fs.createWriteStream(`./data/rec/${rID}.zip`, {flags: "wx"});
+    
+    MFBAcvr.logger.log(`Started cooking the recording file [Recording ID: ${rID}]`);
+    
+    let output = fs.createWriteStream(`./data/rec/${rID}.zip`, {flags: "wx"}),
+        errorOutput = fs.createWriteStream(`./data/rec/${rID}.cook.log`, {flags: "wx"});
     let cook;
     if (process.platform === 'win32'){
       // Windows Subsystem for Linux
       cook = cp.spawn("bash", ["./audio-cooker/cook.sh", rID], {
         cwd: path.dirname(require.main.filename),
-        stdio: ["ignore", "pipe", "ignore"]
+        stdio: ["ignore", "pipe", "pipe"]
       });
     } else{
       cook = cp.spawn("./audio-cooker-cook.sh", [rID], {
         cwd: path.dirname(require.main.filename),
-        stdio: ["ignore", "pipe", "ignore"]
+        stdio: ["ignore", "pipe", "pipe"]
       });
     }
     
     cook.stdout.pipe(output);
+    cook.stderr.pipe(errorOutput);
     cook.on("exit", (code, signal) => {
       output.end();
+      errorOutput.end();
+      MFBAcvr.logger.log(`Finished cooking the recording file [Recording ID: ${rID}]`);
     });
   };
 }
