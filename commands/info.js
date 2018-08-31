718e86cb36a3b7d837b0a48d4c7d42c6846db74d
MaySoMusician <maysomusician@gmail.com>
2018-08-31 23:30:08 +0900

    Update info command

--- commands/info.js
+++ commands/info.js
@@ -3,7 +3,7 @@ const moment = require("moment");
 require("moment-duration-format");
 
 exports.run = (MFBAcvr, message, args) => { // eslint-disable-line no-unused-vars
-  const duration = moment.duration(MFBAcvr.uptime).format(" D [日], H [時間], m [分], s [秒]");
+  const duration = moment.duration(MFBAcvr.uptime).format(" D [日] H [時間] m [分] s [秒]");
   message.channel.send(`= MFB-Archiver 統計 =
 • メモリ使用量 :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
 • 稼働時間　　 :: ${duration}
@@ -16,7 +16,7 @@ exports.run = (MFBAcvr, message, args) => { // eslint-disable-line no-unused-var
 = About MFB-Archiver =
 MFB-Archiver is mainly developed by MaySoMusician, and is
 available at https://github.com/MaySoMusician/MFB-Archiver
-as a open-source project.
+as an open-source project.
 You can fix a bug or add a new feature you want by forking
 the repository and sending a pull request from yours,
 or report/discuss problems by opening a issue.
